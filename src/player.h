73b85b7f97c2c46dff199f795397308aa5209496
Exant <warpcauldronproject@gmail.com>
2025-10-13 21:25:07 +0200

    forgot to push player.h changes for al_chao_info commit

--- src/player.h
+++ src/player.h
@@ -3,6 +3,48 @@
 
 #include <task.h>
 
+typedef struct CharObj2Base
+{
+  char PlayerNum;
+  char CharID;
+  char Costume;
+  char CharID2;
+  char ActionWindowItems[8];
+  char ActionWindowItemCount;
+  char field_D[3];
+  Sint16 Powerups;
+  Sint16 field_12;
+  Sint16 DisableControlTimer;
+  Sint16 UnderwaterTime;
+  Sint16 IdleTime;
+  Sint16 ConfuseTime;
+  char gap1C[8];
+  int Upgrades;
+  float field_28;
+  int field_2C;
+  float PathDist;
+  float Up;
+  char field_38[8];
+  float SomeMultiplier;
+  int field_44;
+  float MechHP;
+  NJS_POINT3 Eff;
+  NJS_POINT3 Acceleration;
+  NJS_VECTOR Speed;
+  NJS_POINT3 WallNormal;
+  NJS_POINT3 FloorNormal;
+  int CurrentSurfaceFlags;
+  int PreviousSurfaceFlags;
+  void *DynColInfo;
+  task *HeldObject;
+  char gap98[4];
+  task *HoldTarget;
+} CharObj2Base;
+
 extern taskwk* playertwp[];
+extern CharObj2Base* playerpwp[];
+
+void HoldTaskP(int, task*);
+int GetAngToPlayer(task*, int index);
 
 #endif
