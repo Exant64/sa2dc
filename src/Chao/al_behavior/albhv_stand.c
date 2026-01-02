b15ffafc6282ef399ff80a108e9a7abd72df9e6c
Exant64 <warpcauldronproject@gmail.com>
2026-01-02 17:29:31 +0200

    albhv_stand: 100%

--- src/Chao/al_behavior/albhv_stand.c
+++ src/Chao/al_behavior/albhv_stand.c
@@ -1,13 +1,133 @@
-#include <task.h>
+#include <Chao/Chao.h>
 
-INLINE_ASM(_ALBHV_Think, 0x17a, "asm/nonmatching/Chao/al_behavior/albhv_stand/_ALBHV_Think.src");
+int ALBHV_Think(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
 
-// MERGE_LIST([['_AL_FaceChangeEye', '_lbl_0C5383E0'], ['_AL_FaceChangeMouth', '_lbl_0C5383E4'], ['_rand', '_lbl_0C5383E8'], ["h'3B720000", '_lbl_0C5383EC'], ["h'3F4CCCCD", '_lbl_0C5383F0'], ['_AL_Brake', '_lbl_0C5383F4']]);
-INLINE_ASM(_func_0C53831A, 0x8e, "asm/nonmatching/Chao/al_behavior/albhv_stand/_func_0C53831A.src");
+    switch(bhv->Mode) {
+        case 0: {
+            int posture = AL_GetMotionPosture(tp);
+            switch(posture) {
+                case 2:
+                case 1:
+                case 3:
+                    AL_SetMotionLinkStep(tp, ALM_HIMA_SIT, 35);
+                    break;
+                case 0:
+                default: {
+                    float randval = njRandom();
+                    if(randval < 0.15f) {
+                        AL_SetMotionLink(tp, ALM_HATENA_HATE2);
+                        AL_IconSet(tp, MD_ICON_HATENA, 180);
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C538400'], ["h'39A00000", '_lbl_0C538404'], ['_rand', '_lbl_0C5383E8'], ['_AL_IsMotionEnd', '_lbl_0C538408'], ["h'38000000", '_lbl_0C5383F8'], ["h'3F000000", '_lbl_0C5383FC']]);
-INLINE_ASM(_func_0C5383A8, 0xc0, "asm/nonmatching/Chao/al_behavior/albhv_stand/_func_0C5383A8.src");
+                        sub_8C05B020(24604, 0, 0, 110, &tp->twp->pos);
+                    }
+                    else if (randval < 0.5f) {
+                        AL_SetMotionLink(tp, ALM_STAND);
+                    }
+                    else if (randval < 0.6f) {
+                        AL_SetMotionLink(tp, ALM_KYOROKYORO_UP);
+                    }
+                    else if (randval < 0.7f) {
+                        AL_SetMotionLink(tp, ALM_KYOROKYORO_DOWN);
+                    }
+                    else {
+                        AL_SetMotionLink(tp, ALM_OSYABURI);
+                    }
+                    
+                    break;
+                }
+            }
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C5384E0'], ["h'39A00000", '_lbl_0C5384E8'], ['_rand', '_lbl_0C5384E4'], ['_AL_IsMotionEnd', '_lbl_0C5384EC']]);
-INLINE_ASM(_func_0C538468, 0x98, "asm/nonmatching/Chao/al_behavior/albhv_stand/_func_0C538468.src");
+            AL_FaceChangeEye(tp, AL_EYE_NUM_NORMAL);
+            AL_FaceChangeMouth(tp, AL_MOUTH_NUM_NONE);
 
+            bhv->Timer = RAND_RANGE(60, 180);
+            bhv->Mode++;
+        }
+
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                return BHV_RET_THINK;
+            }
+            
+            break;
+    }
+
+    AL_Brake(tp, 0.8f);
+    
+    return BHV_RET_CONTINUE;
+}
+
+// dunno what these do yet
+// best guess I have is that its somehow kindergarten related
+
+int ALBHV_Think2(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            if(njRandom() < 0.5f) {
+                AL_SetMotionLink(tp, ALM_KYOROKYORO_UP);
+            }
+            else {
+                AL_SetMotionLink(tp, ALM_KYOROKYORO_DOWN);
+            }
+
+            bhv->Mode++;
+            bhv->Timer = 5 + (Uint16)(njRandom() * 10);
+        case 1:
+            if(AL_IsMotionEnd(tp)) {
+                if(!--bhv->Timer) {
+                    return BHV_RET_FINISH;
+                }
+            }
+            
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Think3(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_STAND);
+
+            bhv->Mode++;
+            bhv->Timer = 5 + (Uint16)(njRandom() * 10);
+        case 1:
+            if(AL_IsMotionEnd(tp)) {
+                if(!--bhv->Timer) {
+                    return BHV_RET_FINISH;
+                }
+            }
+            
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Think4(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_OSYABURI);
+
+            bhv->Mode++;
+            bhv->Timer = 10 + (Uint16)(njRandom() * 10);
+        case 1:
+            if(AL_IsMotionEnd(tp)) {
+                if(!--bhv->Timer) {
+                    return BHV_RET_FINISH;
+                }
+            }
+            
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
