839b37500246ad50320a0953de2fb05d879b4919
Exant64 <warpcauldronproject@gmail.com>
2026-01-13 10:36:44 +0100

    albhv_betafuncs progress uploaded (help needed, scratch linked in file)

--- src/Chao/al_behavior/albhv_betafuncs.c
+++ src/Chao/al_behavior/albhv_betafuncs.c
@@ -1,21 +1,249 @@
-#include <task.h>
+#include <Chao/Chao.h>
 
-INLINE_ASM(_func_0C53B4A0, 0x94, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B4A0.src");
+/*
+    really weird file
+    first off, did they use extern like this too to access albhv_music's models?
+    the data isnt decomped yet but these functions have a table with an accompanying string table
+    none of these functions ever finish
+    and the instrument bhvs are slightly older versions of the real ones or something
 
-// MERGE_LIST([['_rand', '_lbl_0C53B5E0'], ["h'38000000", '_lbl_0C53B5E4'], ["h'3ECCCCCD", '_lbl_0C53B5E8'], ['_AL_SetMotionLink', '_lbl_0C53B5EC'], ['_AL_FaceSetEye', '_lbl_0C53B5F0'], ['_AL_FaceSetMouth', '_lbl_0C53B5F4'], ["h'3B720000", '_lbl_0C53B5F8']]);
-INLINE_ASM(_func_0C53B534, 0xcc, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B534.src");
+    beta tambourine (last func) is on decomp.me, it somehow includes unreachable code
+    which shc never does so i have no idea how to match it
+*/
 
-INLINE_ASM(_func_0C53B600, 0x84, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B600.src");
+extern NJS_CNK_OBJECT object_alo_maracas_pos_l_maracas1[];
+extern NJS_CNK_OBJECT object_alo_maracas_pos_r_maracas[];
+extern NJS_CNK_OBJECT object_alo_dram_pos_l_batil[];
+extern NJS_CNK_OBJECT object_alo_dram_pos_r_batir[];
+extern NJS_CNK_OBJECT object_alo_dram_pos_c_dram[];
+extern NJS_CNK_OBJECT object_alo_sinbal_pos_l_sinbal2[];
+extern NJS_CNK_OBJECT object_alo_sinbal_pos_r_sinbal1[];
+extern NJS_CNK_OBJECT object_alo_tambarin_pos_tambarin[];
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C53B760'], ['_AL_FaceSetEye', '_lbl_0C53B764'], ['_rand', '_lbl_0C53B768'], ["h'38000000", '_lbl_0C53B76C'], ["h'3F000000", '_lbl_0C53B770'], ['_AL_FaceSetMouth', '_lbl_0C53B774'], ['_AL_IsMotionStop', '_lbl_0C53B778']]);
-INLINE_ASM(_func_0C53B684, 0x186, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B684.src");
+int ALBHV_BetaOjigi(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
 
-// MERGE_LIST([['_AL_GetMotionNum', '_lbl_0C53B900'], ["h'3AF40000", '_lbl_0C53B904'], ['_AL_SetMotionLinkSync', '_lbl_0C53B908'], ['_AL_SetMotionLinkStep', '_lbl_0C53B90C']]);
-INLINE_ASM(_func_0C53B80A, 0x198, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B80A.src");
+    switch(bhv->Mode) {
+        case 0:
+            if(njRandom() < 0.4f) {
+                AL_SetMotionLink(tp, ALM_OJIGI);
+            }
 
-// MERGE_LIST([['_AL_SetMotionLinkStep', '_lbl_0C53BAE0']]);
-INLINE_ASM(_func_0C53B9A2, 0x17e, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53B9A2.src");
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            AL_FaceSetMouth(tp, AL_MOUTH_NUM_NIKO, -1);
 
+            bhv->Timer = RAND_RANGE(120, 240);
+            bhv->Mode++;
+            break;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                bhv->Mode--;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_BetaLaugh(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            if(njRandom() < 0.4f) {
+                AL_SetMotionLink(tp, ALM_LAUGH_SIT);
+            }
+            else {
+                AL_SetMotionLink(tp, ALM_HIMA_SIT);
+            }
+
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            AL_FaceSetMouth(tp, AL_MOUTH_NUM_NIKO, -1);
+
+            bhv->Timer = RAND_RANGE(60, 120);
+            bhv->Mode++;
+            break;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                bhv->Mode--;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_BetaSing(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_UTAU_LEFT);
+
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            if(njRandom() < 0.5f) {
+                AL_FaceSetMouth(tp, AL_MOUTH_NUM_NIKO, -1);
+            }
+            else {
+                AL_FaceSetMouth(tp, AL_MOUTH_NUM_NONE, -1);
+            }
+
+            bhv->Mode++;
+            break;
+        case 1:
+            if(AL_IsMotionStop(tp)) {
+                bhv->Mode--;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+// same thing as the original but with sounds
+// (and without finishing)
+int ALBHV_BetaMaracas(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+    int mot_num;
+
+    switch (bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_MARACAS_SHAKA_UP);
+
+            AL_FaceSetEye(tp, AL_EYE_NUM_KYA, -1);
+            AL_FaceSetMouth(tp, AL_MOUTH_NUM_SHARK, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_L, object_alo_maracas_pos_l_maracas1, &AL_TOY_TEXLIST);
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_maracas_pos_r_maracas, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(300, 600);
+            ++bhv->Mode;
+        case 1:
+            if (bhv->Timer-- <= 0 || AL_IsMotionStop(tp)) {
+                if (njRandom() < 0.3f) {
+                    AL_SetMotionLinkStep(tp, ALM_MARACAS_JUMP_B, 10);
+                } else {
+                    mot_num = AL_GetMotionNum(tp);
+                    switch (mot_num) {
+                        case ALM_MARACAS_SHAKA_UP:
+                            AL_SetMotionLinkSync(tp, ALM_MARACAS_SHAKA_DOWN);
+                            bhv->Timer = RAND_RANGE(60, 120);
+                            break;
+                        case ALM_MARACAS_SHAKA_DOWN:
+                            AL_SetMotionLinkSync(tp, ALM_MARACAS_SHAKA_UP);
+                            bhv->Timer = RAND_RANGE(60, 120);
+                            break;
+                        default:
+                            AL_SetMotionLinkStep(tp, ALM_MARACAS_SHAKA_UP, 10);
+                            bhv->Timer = RAND_RANGE(60, 120);
+                            break;
+                    }
+                }
+            }
+            break;
+        default:
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+// same thing as real one but without sound handler at bottom and with if-else hack for GetMotionNum regswap
+// (and without finishing)
+int ALBHV_BetaDrum(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+    int mot_num;
+
+    switch (bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_DRUM_LR);
+
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            AL_FaceSetMouth(tp, AL_MOUTH_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_L, object_alo_dram_pos_l_batil, &AL_TOY_TEXLIST);
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_dram_pos_r_batir, &AL_TOY_TEXLIST);
+            AL_SetItem(tp, AL_PART_NAVEL, object_alo_dram_pos_c_dram, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(300, 600);
+            ++bhv->Mode;
+        case 1:
+            if (bhv->Timer-- > 0) {} else {
+                mot_num = AL_GetMotionNum(tp);
+                switch (mot_num) {
+                    case ALM_DRUM_LR:
+                        if (njRandom() < 0.6f) {
+                            AL_SetMotionLinkSync(tp, ALM_DRUM_LR);
+                        } else {
+                            AL_SetMotionLinkStep(tp, ALM_DRUM_DOUBLE, 10);
+                        }
+
+                        bhv->Timer = RAND_RANGE(60, 240);
+                        break;
+                    case ALM_DRUM_DOUBLE:
+                        AL_SetMotionLinkStep(tp, ALM_DRUM_LR, 10);
+
+                        bhv->Timer = RAND_RANGE(60, 240);
+                        break;
+                }
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+// no sounds but more anims! (+ if-else hack)
+int ALBHV_BetaCymbal(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+    int mot_num;
+
+    switch (bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_CYMBAL_WAIT);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            AL_FaceSetMouth(tp, AL_MOUTH_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_L, object_alo_sinbal_pos_l_sinbal2, &AL_TOY_TEXLIST);
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_sinbal_pos_r_sinbal1, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(300, 600);
+            ++bhv->Mode;
+        case 1:
+            if (bhv->Timer-- > 0) {} else {
+                mot_num = AL_GetMotionNum(tp);
+                switch (mot_num) {
+                    case ALM_CYMBAL_WAIT:
+                        if(njRandom() < 0.6f) {
+                            if(njRandom() < 0.5f) {
+                                AL_SetMotionLinkStep(tp, ALM_CYMBAL_PAN, 10);
+                            }
+                            else {
+                                AL_SetMotionLinkStep(tp, ALM_CYMBAL_PANPAN, 10);
+                            }
+                        }
+                        else {
+                            AL_SetMotionLinkStep(tp, ALM_CYMBAL_PAPAPAN, 10);
+                        }
+
+                        bhv->Timer = RAND_RANGE(60, 240);
+                        break;
+                    default:
+                        AL_SetMotionLinkStep(tp, ALM_CYMBAL_WAIT, 10);
+
+                        bhv->Timer = RAND_RANGE(60, 240);
+                        break;
+                }
+            }
+
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+// https://decomp.me/scratch/g4awI
 INLINE_ASM(_func_0C53BB20, 0xe0, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53BB20.src");
 
 INLINE_ASM(_func_0C53BC00, 0x28, "asm/nonmatching/Chao/al_behavior/albhv_betafuncs/_func_0C53BC00.src");
