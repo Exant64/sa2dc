218c43a702b10dd94f44e0c2e7300c0d08ac4fa1
Exant64 <warpcauldronproject@gmail.com>
2026-01-13 10:28:51 +0100

    albhv_toy: 100% (all used chao behaviors matched!)

--- src/Chao/al_behavior/albhv_toy.c
+++ src/Chao/al_behavior/albhv_toy.c
@@ -1,50 +1,479 @@
-#include <task.h>
+#include <Chao/Chao.h>
 
-INLINE_ASM(_ALO_ShabonDisplayer, 0x94, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALO_ShabonDisplayer.src");
+#include <effect2.h>
 
-// MERGE_LIST([['_njColorBlendingMode', '_lbl_0C5393C0'], ["h'8C11B96C", '_lbl_0C5393E0'], ['_AL_TOY_TEXLIST', '_lbl_0C5393C4'], ['_njPopMatrixEx', '_lbl_0C5393E4'], ['_njSetTexture', '_lbl_0C5393C8'], ['_SetMaterial', '_lbl_0C5393CC'], ['_anim', '_lbl_0C5393D0'], ["h'3E4CCCCD", '_lbl_0C5393D4'], ['_njPushMatrixEx', '_lbl_0C5393D8'], ['_njTranslateEx', '_lbl_0C5393DC']]);
-INLINE_ASM(_ALO_ShabonExecutor, 0xa2, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALO_ShabonExecutor.src");
+extern NJS_TEXANIM anim[];
 
-// MERGE_LIST([["h'3C23D70A", '_lbl_0C539400'], ["h'3CA3D70A", '_lbl_0C539404'], ['_njScalor', '_lbl_0C5393E8'], ['_MOV_Control', '_lbl_0C539408'], ["h'3D4CCCCD", '_lbl_0C5393EC'], ["h'3F666666", '_lbl_0C5393F0'], ["h'3E19999A", '_lbl_0C5393F4'], ['_FreeTask', '_lbl_0C5393F8'], ['_njSin', '_lbl_0C5393FC']]);
-INLINE_ASM(_ALO_ShabonCreate, 0xb2, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALO_ShabonCreate.src");
+extern NJS_CNK_OBJECT object_alo_syabon_bottle_bottle[];
+extern NJS_CNK_OBJECT object_alo_syabon_stllow_stllow[];
+extern NJS_CNK_OBJECT object_alo_houki_pos_pos[];
+extern NJS_CNK_OBJECT object_alo_gara_n_gara_n[];
+extern NJS_CNK_OBJECT object_alo_hanabi_h_hanabi_h[];
+extern NJS_CNK_OBJECT object_alo_book_pos_pos[];
+extern NJS_MOTION motion_alo_book_pos[];
+extern NJS_CNK_OBJECT object_alo_minicar_minicar[];
+extern NJS_CNK_OBJECT object_alo_doll_doll[];
+extern NJS_CNK_OBJECT object_alo_hopping_hopping[];
 
-// MERGE_LIST([['_MOV_SetGravity', '_lbl_0C539420'], ["h'40400000", '_lbl_0C539424'], ["h'3F4CCCCD", '_lbl_0C539428'], ['_lbl_0C5672FC', '_lbl_0C53940C'], ["h'3F333333", '_lbl_0C53942C'], ['_ALO_ShabonExecutor', '_lbl_0C539410'], ['_rand', '_lbl_0C539430'], ['_CreateElementalTask', '_lbl_0C539414'], ["h'3799999A", '_lbl_0C539434'], ['_ALO_ShabonDisplayer', '_lbl_0C539418'], ['_MOV_Init', '_lbl_0C53941C']]);
-INLINE_ASM(_ALBHV_Syabon, 0x200, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Syabon.src");
+extern BHV_FUNC ALBHV_SToyFunc[];
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C539620'], ["h'38000000", '_lbl_0C539640'], ["h'3E800000", '_lbl_0C539660'], ['_AL_FaceSetEye', '_lbl_0C539624'], ["h'3ECCCCCD", '_lbl_0C539644'], ['_AL_TOY_TEXLIST', '_lbl_0C539628'], ["h'364CCCCD", '_lbl_0C539648'], ["h'3D4CCCCD", '_lbl_0C539658'], ['_lbl_0C632400', '_lbl_0C53962C'], ["h'3E4CCCCD", '_lbl_0C53964C'], ['_AL_SetItem', '_lbl_0C539630'], ["h'3DCCCCCD", '_lbl_0C539650'], ['_lbl_0C632628', '_lbl_0C539634'], ["h'3F000000", '_lbl_0C539654'], ['_rand', '_lbl_0C539638'], ['_AL_IsMotionStop', '_lbl_0C53965C'], ['__modls', '_lbl_0C53963C']]);
-INLINE_ASM(_ALBHV_Houki, 0xf8, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Houki.src");
+void ALO_ShabonDisplayer(task* tp) {
+    taskwk* twk = tp->twp;
+    float scl = (0.2f * twk->scl.x) * twk->scl.y;
+    NJS_SPRITE sprite;
+    
+    njColorBlendingMode(0, 8);
+    njColorBlendingMode(1, 10);
 
-INLINE_ASM(_ALBHV_Garagara, 0x82, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Garagara.src");
+    njSetTexture(&AL_TOY_TEXLIST);
+    SetMaterial(1, 1, 1, 1);
+    
+    sprite.tlist = &AL_TOY_TEXLIST;
+    sprite.tanim = anim;
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C5397C0'], ['_AL_FaceSetEye', '_lbl_0C5397C4'], ['_AL_TOY_TEXLIST', '_lbl_0C5397C8'], ['_object_alo_gara_n_gara_n', '_lbl_0C5397CC'], ['_AL_SetItem', '_lbl_0C5397D0'], ['_rand', '_lbl_0C5397D4'], ["h'3BC90000", '_lbl_0C5397D8']]);
-INLINE_ASM(_ALBHV_Hanabi, 0x82, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Hanabi.src");
+    sprite.ang = 0;
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C5397C0'], ['_AL_FaceSetEye', '_lbl_0C5397C4'], ['_AL_TOY_TEXLIST', '_lbl_0C5397C8'], ['_AL_SetItem', '_lbl_0C5397D0'], ['_rand', '_lbl_0C5397D4'], ["h'3BC90000", '_lbl_0C5397D8'], ['_object_alo_hanabi_h_hanabi_h', '_lbl_0C5397DC']]);
-INLINE_ASM(_BookExecutor, 0xd4, "asm/nonmatching/Chao/al_behavior/albhv_toy/_BookExecutor.src");
+    sprite.sx = 0.2f;
+    sprite.sy = 0.2f;
 
-// MERGE_LIST([["h'41700000", '_lbl_0C539920'], ["h'3E19999A", '_lbl_0C539924'], ["h'41B80000", '_lbl_0C539928'], ['_FreeTask', '_lbl_0C53992C']]);
-INLINE_ASM(_BookDisplayer, 0x5e, "asm/nonmatching/Chao/al_behavior/albhv_toy/_BookDisplayer.src");
+    sprite.p.x = 0;
+    sprite.p.y = 0;
+    sprite.p.z = 0;
 
-// MERGE_LIST([['_njTranslateV', '_lbl_0C539940'], ['_njPopMatrixEx', '_lbl_0C539958'], ['_njRotateY', '_lbl_0C539944'], ['_njScale', '_lbl_0C539948'], ['_motion_alo_book_pos', '_lbl_0C53994C'], ["h'3C23D70A", '_lbl_0C539930'], ['_object_alo_book_pos_pos', '_lbl_0C539950'], ['_AL_TOY_TEXLIST', '_lbl_0C539934'], ['_njCnkEasyDrawMotion', '_lbl_0C539954'], ['_njSetTexture', '_lbl_0C539938'], ['_njPushMatrixEx', '_lbl_0C53993C']]);
-INLINE_ASM(_BookDestructor, 0x4, "asm/nonmatching/Chao/al_behavior/albhv_toy/_BookDestructor.src");
+    njPushMatrixEx();
+    njTranslateEx(&twk->pos);
+    njDrawSprite3D(&sprite, 0, NJD_SPRITE_SCALE | NJD_SPRITE_ALPHA);
+    njPopMatrixEx();
 
-INLINE_ASM(_BookCreate, 0xc6, "asm/nonmatching/Chao/al_behavior/albhv_toy/_BookCreate.src");
+    SetMaterial(0, 0, 0, 0);
 
-INLINE_ASM(_ALBHV_Book, 0x11a, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Book.src");
+    njColorBlendingMode(0, 8);
+    njColorBlendingMode(1, 6);
+}
 
-// MERGE_LIST([["h'38000000", '_lbl_0C539AE0'], ['_AL_IsMotionStop', '_lbl_0C539B00'], ['_AL_SetMotionLink', '_lbl_0C539AE4'], ["h'3BB50000", '_lbl_0C539B04'], ['_rand', '_lbl_0C539AE8'], ['_AL_FixPosition', '_lbl_0C539AEC'], ["h'3F000000", '_lbl_0C539AF0'], ['_AL_FaceSetEye', '_lbl_0C539AF4'], ["h'3B4A0000", '_lbl_0C539AF8'], ["h'3E99999A", '_lbl_0C539AFC']]);
-INLINE_ASM(_ALBHV_Minicar, 0x178, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Minicar.src");
+void ALO_ShabonExecutor(task* tp) {
+    taskwk* work = tp->twp;
+    MOVE_WORK* move = tp->Data2;
 
-// MERGE_LIST([['_AL_TOY_TEXLIST', '_lbl_0C539CA0'], ['_object_alo_minicar_minicar', '_lbl_0C539CA4'], ['_AL_SetItemParallelRightHand', '_lbl_0C539CA8'], ["h'3B4A0000", '_lbl_0C539CAC'], ["h'3E4CCCCD", '_lbl_0C539CB0'], ['_AL_IsMotionStop', '_lbl_0C539CB4'], ["h'3BB50000", '_lbl_0C539CB8']]);
-INLINE_ASM(_ALBHV_Doll, 0xe6, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Doll.src");
+    if(njScalor(&move->Velo) > 0.05f) {
+        move->Velo.x *= 0.9f;
+        move->Velo.y *= 0.9f;
+        move->Velo.z *= 0.9f;
+    }
 
-INLINE_ASM(_ALBHV_Hopping, 0x80, "asm/nonmatching/Chao/al_behavior/albhv_toy/_ALBHV_Hopping.src");
+    work->scl.y += 0.15f;
+    if(work->scl.y > 1) {
+        work->scl.y = 0;
+    }
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C539E80'], ['_AL_FaceSetEye', '_lbl_0C539E84'], ['_AL_TOY_TEXLIST', '_lbl_0C539E88'], ['_object_alo_hopping_hopping', '_lbl_0C539E8C'], ['_AL_SetItem', '_lbl_0C539E90'], ['_rand', '_lbl_0C539E94'], ["h'3C168000", '_lbl_0C539E98']]);
-INLINE_ASM(_AL_DecideBehaviorSToy, 0x164, "asm/nonmatching/Chao/al_behavior/albhv_toy/_AL_DecideBehaviorSToy.src");
+    if(work->wtimer++ > 1800) {
+        FreeTask(tp);
+    }
 
-INLINE_ASM(_func_0C539EDC, 0x34, "asm/nonmatching/Chao/al_behavior/albhv_toy/_func_0C539EDC.src");
+    work->pos.y += njSin(work->ang.y) * 0.02f + 0.01f;
+    work->ang.y += 256;
 
-// MERGE_LIST([['_AL_SetMotionLink', '_lbl_0C539FE0'], ['_rand', '_lbl_0C539FE4']]);
-INLINE_ASM(_rodata, "asm/nonmatching/Chao/al_behavior/albhv_toy/rodata.src");
+    MOV_Control(tp);
+}
 
+task* ALO_CreateShabon(NJS_POINT3 *pPos, NJS_VECTOR *pVelo) {
+    task* tp = CreateElementalTask(2, 4, ALO_ShabonExecutor, "ALO_ShabonExecutor");
+    taskwk* twk = tp->twp;
+    MOVE_WORK* move;
+
+    tp->disp = ALO_ShabonDisplayer;
+
+    move = MOV_Init(tp);
+
+    twk->pos = *pPos;
+
+    if(pVelo) {
+        move->Velo = *pVelo;
+    }
+
+    MOV_SetGravity(tp, 0.f);
+
+    move->Offset.y = 3;
+    move->BoundFloor = 0.8f;
+    move->BoundFriction = 0.7f;
+
+    twk->ang.y = NJM_DEG_ANG(njRandom() * 360.f);
+
+    twk->scl.x = 0.7f + 0.6f * njRandom();
+    twk->scl.y = 0;
+
+    // they forgot to return
+}
+
+int ALBHV_Syabon(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_SYABON_JYABU);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_L, object_alo_syabon_bottle_bottle, &AL_TOY_TEXLIST);
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_syabon_stllow_stllow, &AL_TOY_TEXLIST);
+
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer++ > 60) {
+                if(!(bhv->Timer % 10) && njRandom() < 0.4f) {
+                    AL_SHAPE* pShape = &GET_CHAOWK(tp)->Shape;
+
+                    NJS_POINT3 pos;
+                    NJS_VECTOR velo;
+
+                    float f = njRandom() * 0.1f + 0.2f;
+
+                    velo.x = pShape->MouthVec.x * f + njRandom() * 0.1f;
+                    velo.y = pShape->MouthVec.y * f + 0.1f;
+                    velo.z = pShape->MouthVec.z * f + njRandom() * 0.1f;
+
+                    pos.x = pShape->RightHandPos.x + pShape->MouthVec.x * 0.5f;
+                    pos.y = pShape->RightHandPos.y + pShape->MouthVec.y * 0.5f + 0.05f;
+                    pos.z = pShape->RightHandPos.z + pShape->MouthVec.z * 0.5f;
+
+                    ALO_CreateShabon(&pos, &velo);
+                }
+            }
+
+            if(AL_IsMotionStop(tp)) {
+                // sadx seems to have changed this to guarantee
+                // atleast 4 runs (counting them here using SubTimer)
+                if(njRandom() < 0.25f) {
+                    return BHV_RET_FINISH;
+                }
+                else {
+                    AL_SetMotionLink(tp, ALM_SYABON_JYABU);
+                    bhv->Timer = 0;
+                }
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Houki(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_HOUKI);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_houki_pos_pos, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(600, 800);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                return BHV_RET_FINISH;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Garagara(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_GARAGARA);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_gara_n_gara_n, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(600, 800);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                return BHV_RET_FINISH;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE; 
+}
+
+int ALBHV_Hanabi(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_HANABI);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_hanabi_h_hanabi_h, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(600, 800);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                return BHV_RET_FINISH;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE; 
+}
+
+enum {
+    MD_BOOK_EXPAND = 0,
+    MD_BOOK_OPEN = 1,
+    MD_BOOK_MEKURI = 2,
+    MD_BOOK_MEKURI2 = 3,
+    MD_BOOK_SHRINK = 4,
+};
+
+void BookExecutor(task* tp) {
+    taskwk* twk = tp->twp;
+
+    switch(twk->mode) {
+        case MD_BOOK_EXPAND:
+            twk->scl.y += 0.1f;
+            if(twk->scl.y > 1) {
+                twk->scl.y = 1;
+                twk->mode = MD_BOOK_OPEN;
+            }
+            break;
+        
+        case MD_BOOK_OPEN:
+            twk->scl.x = 15;
+            break;
+
+        case MD_BOOK_MEKURI:
+            twk->scl.x += 0.15f;
+            if(twk->scl.x > 23) {
+                twk->scl.x = 23;
+                twk->mode = MD_BOOK_OPEN;
+            }
+            break;
+
+        case MD_BOOK_SHRINK:
+            twk->scl.y -= 0.1f;
+            if(twk->scl.y < 0) {
+                twk->scl.y = 0;
+                FreeTask(tp);
+            }
+            break;
+    }
+}
+
+void BookDisplayer(task* tp) {
+    taskwk* twk = tp->twp;
+    float scl = twk->scl.y;
+
+    if(scl >= 0.01f) {
+        njSetTexture(&AL_TOY_TEXLIST);
+
+        njPushMatrixEx();
+
+        njTranslateV(NULL, &twk->pos);
+        njRotateY(NULL, twk->ang.y);
+        njScale(NULL, scl, scl, scl);
+
+        njCnkEasyDrawMotion(object_alo_book_pos_pos, motion_alo_book_pos, twk->scl.x);
+
+        njPopMatrixEx();
+    }
+}
+
+void BookDestructor(task* tp) {
+
+}
+
+void BookCreate(task* pChaoTask) {
+    task* tp;
+
+    if(GET_CHAOWK(pChaoTask)->pBookTask) {
+        FreeTask(GET_CHAOWK(pChaoTask)->pBookTask);
+    }
+
+    tp = CreateElementalTask(2, 2, BookExecutor, "BookExecutor");
+    GET_CHAOWK(pChaoTask)->pBookTask = tp;
+
+    tp->twp->pos = pChaoTask->twp->pos;
+    tp->twp->ang.y = pChaoTask->twp->ang.y;
+
+    tp->disp = BookDisplayer;
+    tp->dest = BookDestructor;
+}
+
+int ALBHV_Book(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch (bhv->Mode) {
+        case 0:
+            AL_FixPosition(tp);
+            AL_SetMotionLink(tp, ALM_BOOK_START);
+
+            if(njRandom() < 0.5f) {
+                AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            }
+
+            bhv->Timer = RAND_RANGE(300, 400);
+            BookCreate(tp);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                if(njRandom() < 0.3f) {
+                    GET_CHAOWK(tp)->pBookTask->twp->mode = MD_BOOK_SHRINK;
+
+                    return BHV_RET_FINISH;
+                }
+                else {
+                    GET_CHAOWK(tp)->pBookTask->twp->mode = MD_BOOK_MEKURI;
+
+                    AL_SetMotionLink(tp, ALM_BOOK_MEKURI);
+                    bhv->Mode++;
+                }
+            }
+            break;
+
+        case 2:
+            if(AL_IsMotionStop(tp)) {
+                AL_SetMotionLink(tp, ALM_BOOK_YOMU);
+
+                bhv->Timer = RAND_RANGE(120, 300);
+                bhv->Mode--;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Minicar(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch (bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_MINICAR);
+
+            if(njRandom() < 0.5f) {
+                AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+            }
+
+            AL_SetItemParallelRightHand(tp, object_alo_minicar_minicar, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(300, 400);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                if(njRandom() < 0.2f) {
+                    return BHV_RET_FINISH;
+                }
+                else {
+                    if(njRandom() < 0.5f) {
+                        AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+                    }
+
+                    AL_SetMotionLink(tp, ALM_MINICAR_MIAGE);
+                    bhv->Mode++;
+                }
+            }
+            break;
+
+        case 2:
+            if(AL_IsMotionStop(tp)) {
+                AL_SetMotionLink(tp, ALM_MINICAR);
+
+                if(njRandom() < 0.5f) {
+                    AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+                }
+
+                bhv->Timer = RAND_RANGE(380, 560);
+                bhv->Mode--;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE;
+}
+
+int ALBHV_Doll(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_DOLL_YOSHIYOSHI);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_L, object_alo_doll_doll, &AL_TOY_TEXLIST);
+
+            bhv->Mode++;
+        case 1:
+            if(AL_IsMotionStop(tp)) {
+                if(njRandom() < 0.2f) {
+                    return BHV_RET_FINISH;
+                }
+                else {
+                    if(njRandom() < 0.6f) {
+                        AL_SetMotionLink(tp, ALM_DOLL_YOSHIYOSHI);
+                    }
+                    else {
+                        AL_SetMotionLink(tp, ALM_DOLL_TAKAITAKAI);
+                    }
+                }
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE; 
+}
+
+int ALBHV_Hopping(task* tp) {
+    AL_BEHAVIOR* bhv = &GET_CHAOWK(tp)->Behavior;
+
+    switch(bhv->Mode) {
+        case 0:
+            AL_SetMotionLink(tp, ALM_HOPPING);
+            AL_FaceSetEye(tp, AL_EYE_NUM_NIKO, -1);
+
+            AL_SetItem(tp, AL_PART_HAND_R, object_alo_hopping_hopping, &AL_TOY_TEXLIST);
+
+            bhv->Timer = RAND_RANGE(600, 900);
+            bhv->Mode++;
+        case 1:
+            if(bhv->Timer-- <= 0) {
+                return BHV_RET_FINISH;
+            }
+            break;
+    }
+
+    return BHV_RET_CONTINUE; 
+}
+
+Bool AL_DecideBehaviorSToy(task* tp) {
+    float score = 0;
+    taskwk* work = tp->twp;
+
+    int BhvList[32];
+    int count = 0;
+
+    int i;
+
+    for(i = 0; i < NB_AL_STOY; i++) {
+        if(AL_KW_IsSToyFlagOn(tp, i)) {
+            if(ALBHV_SToyFunc[i]) {
+                BhvList[count] = i;
+                count++;
+            }
+        }
+    }
+
+    if(count > 0) {
+        int num = BhvList[(Uint32)((count - 0.0001f) * njRandom())];
+        
+        AL_SetBehavior(tp, ALBHV_SToyFunc[num]);
+
+        return TRUE;
+    }
+
+    return FALSE;
+}
